c49c5eeeadcfc8933b0cbb4e8d9b8cbc06aa98e2
Robert Benavidez <murabitbenavidez@gmail.com>
2019-06-20 01:25:55 -0400

    movie links work

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -26,7 +26,9 @@ export default class MovieList extends Component {
     return (
       <div className="movie-list">
         {this.state.movies.map(movie => (
-          <MovieCard key={movie.id} movie={movie} />
+          <Link to={`/movies/${movie.id}`}>
+            <MovieCard key={movie.id} movie={movie} />
+          </Link>
         ))}
       </div>
     );
